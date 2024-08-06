43e1ba9ddc7f84388cdc0e27468f07e9fef1c168
Hyewon Jo <gpdnjsl123@naver.com>
2024-08-06 14:47:25 +0900

    이거 맞나?
    TEST

---
+++ CrudExam/HomeworkFEDay1/src/main/java/com/example/demo/HomeworkFeDay1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkFeDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkFeDay1Application.class, args);
+	}
+
+}
