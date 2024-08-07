0c5be162497c24c55dd531336ff0ce833e64b541
Hyewon Jo <gpdnjsl123@naver.com>
2024-08-07 17:22:15 +0900

    숙제 완
    곧 퇴근입니다 ㅋ

---
+++ CrudExam/hwfeday2/src/main/java/com/example/demo/Hwfeday2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hwfeday2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hwfeday2Application.class, args);
+	}
+
+}
